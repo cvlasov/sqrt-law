8ff3064fb7c1b411acffcba6a244e732b92e5756
cvlasov <catherinev.canada@gmail.com>
2018-11-10 18:04:59 +0000

    Add script to compute features using JRM

---
+++ scripts/compute_features.py
@@ -0,0 +1,26 @@
+"""
+This script runs JRM to compute the features of all JPEGs in a directory.
+For each image, called "imageX.jpg" say, a file called "imageX.fea" is
+generated. This file contains one line, which starts with the image's
+filename followed by whitespace and then the whitespace-separates features.
+"""
+
+import argparse
+import os
+import subprocess
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-I", "--input-dir", help="Directory with all images")
+args = parser.parse_args()
+
+start = time.time()
+
+for image_name in os.listdir(args.input_dir):
+  name_without_ext = os.path.splitext(image_name)[0]
+  subprocess.call('./jrm -a ' + args.input_dir + image_name + ' > '
+                  + args.input_dir + name_without_ext + '.fea', shell=True)
+
+end = time.time()
+print('\n------------------------\n')
+print(str(end-start) + ' seconds elapsed')
