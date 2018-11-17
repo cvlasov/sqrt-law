4e509dff6901b8a8f6f8de52fb8bd9048775bab1
cvlasov <catherinev.canada@gmail.com>
2018-11-17 23:43:42 +0000

    Use multithreading to compute costs

--- scripts/compute_costs.py
+++ scripts/compute_costs.py
@@ -8,22 +8,33 @@ row-by-row fashion.
 import argparse
 import os
 import subprocess
+import threading
 import time
+
+num_threads = 10
+
+def run(self):
+  for n in range(0,int(13350/num_threads)):
+    image_num = n * num_threads + thread_id
+    image_partial_path = args.input_dir + 'image' + str(image_num).zfill(5)
+    if os.path.isfile(image_partial_path + '.jpg'):
+      subprocess.call('./J-UNIWARD -v -i ' + image_partial_path + '.jpg'
+                      + ' -O ' + args.output_dir + ' -a 0.4', shell=True)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-I", "--input-dir", required=True,
                     help="Directory with all images")
 parser.add_argument("-O", "--output-dir", required=True,
-                    help="Directory to save feature files")
+                    help="Directory to save cost files")
 args = parser.parse_args()
 
 start = time.time()
+thread_list = []
 
-for image_num in range(1,13350):
-  image_partial_path = args.input_dir + 'image' + str(image_num).zfill(5)
-  if os.path.isfile(image_partial_path + '.jpg'):
-    subprocess.call('./J-UNIWARD -v -i ' + image_partial_path + '.jpg'
-                    + ' -O ' + args.output_dir + ' -a 0.4', shell=True)
+for thread_id in range(0,num_threads):
+  thread = threading.Thread(target=run, args=(thread_id, ))
+  thread.start()
+  thread.join()
 
 end = time.time()
 print('\n------------------------\n')
