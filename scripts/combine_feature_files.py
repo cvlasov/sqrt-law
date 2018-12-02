62ad60b9c7d3b5713b0bd6551f98a88dbe4cf087
cvlasov <catherinev.canada@gmail.com>
2018-12-02 16:21:07 +0000

    Change image path to image name when combining feature files

--- scripts/combine_feature_files.py
+++ scripts/combine_feature_files.py
@@ -21,11 +21,19 @@ args = parser.parse_args()
 start = time.time()
 subprocess.call('touch ' + args.output_file, shell=True)
 
-for image_num in range(1,13350):
-  image_fea_path = args.input_dir + 'image' + str(image_num).zfill(5) + '.fea'
-  if os.path.isfile(image_fea_path):
-    subprocess.call('cat ' + image_fea_path + ' >> ' + args.output_file,
-                    shell=True)
+with open(args.output_file, "a") as combined_feature_file:
+  for image_num in range(1,13350):
+    image_fea_path = args.input_dir + 'image' + str(image_num).zfill(5) + '.fea'
+    if os.path.isfile(image_fea_path):
+      with open(image_fea_path) as feature_file:
+        # Each file has a single line.
+        tokens = feature_file.readline().strip().split()
+        # The first token on each line is the image path. We want this to be
+        # just the image name so that cover and stego image names can be
+        # compared. Without this, the paths would never match since the cover
+        # and stego images are in different directories.
+        tokens[0] = os.path.basename(tokens[0])
+        combined_feature_file.write(" ".join(tokens) + "\n")
 
 end = time.time()
 print('\n------------------------')
